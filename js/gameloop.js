3b6f75d7d3a2ef837fda7b48f8b90af6b9fcf83b
Claudio O. R <claudio.raisch@hotmail.de>
2023-08-26 11:59:22 +0200

    Refactor Indentations and Trailing Whitespaces

--- js/gameloop.js
+++ js/gameloop.js
@@ -4,25 +4,24 @@ class GameLoop {
       this.context = this.canvas.getContext('2d');
       this.tLast;
     }
-  
+
     init() {
       // Start the first frame request
       window.requestAnimationFrame((t) => this.spin(t));
     }
-  
+
     spin(timestamp){
       let tDeltaLoop = (timestamp - this.tLast) / 1000
       this.tLast = timestamp
-    
+
       this.render(tDeltaLoop);
-    
+
       // Keep requesting new frames
       window.requestAnimationFrame((t) => this.spin(t));
     }
-  
+
     setRenderCallback(callback) {
       this.render = callback;
     }
-  
+
   }
-  
